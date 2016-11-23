4903fa93bfc5785ef2812946c77e57834feb3a3b
Tuuu <tu@techmaster.vn>
2016-11-23 11:12:17 +0700

    newCm1

--- Led-master/Led/ViewController.swift
+++ Led-master/Led/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
     var right : Bool = true
     var n = 0;
     
+    //NewCommit1
     override func viewDidLoad() {
         super.viewDidLoad()
         hienthi_run.isEnabled = false
